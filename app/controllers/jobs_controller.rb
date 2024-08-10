0efc8c83f5700ad0d11962e576fb45af5added27
daisuke-shimura <168567272+daisuke-shimura@users.noreply.github.com>
2024-08-10 03:56:34 +0000

    提出2

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,6 +4,13 @@ class JobsController < ApplicationController
     @job = Job.new
   end
 
+  def create
+    @job = Job.new(job_params)
+    @job.user_id = current_user.id
+    @job.save
+    redirect_to jobs_path
+  end
+
   def index
   end
 
@@ -12,4 +19,11 @@ class JobsController < ApplicationController
 
   def edit
   end
+
+
+  private
+  def job_params
+    params.require(:job).permit(:time)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :jobs, only: [:new, :index, :show, :edit]
+  resources :jobs, only: [:new, :create, :index, :show, :edit]
   resources :users, only: [:show, :edit]
 
 end
