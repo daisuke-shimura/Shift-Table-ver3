e79787869c23c836999d8618c798c964b0a03b4a
daisuke-shimura <168567272+daisuke-shimura@users.noreply.github.com>
2024-08-30 16:56:35 +0000

    email消す 途中

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_19_025207) do
+ActiveRecord::Schema.define(version: 2024_08_04_234939) do
 
   create_table "jobs", force: :cascade do |t|
-    t.string "time"
+    t.string "time1"
+    t.string "time2"
+    t.string "time3"
+    t.string "time4"
+    t.string "time5"
+    t.string "time6"
+    t.string "time7"
     t.integer "day"
     t.integer "user_id"
     t.string "remark"
@@ -22,7 +28,7 @@ ActiveRecord::Schema.define(version: 2024_08_19_025207) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
