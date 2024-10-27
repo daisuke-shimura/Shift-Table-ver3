94850af0033f79a3fab46fe629393798caab10d1
daisuke-shimura <daisuke.s0707@icloud.com>
2024-10-27 17:35:31 +0900

    sm_Finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  #before_action :basic_authentication
+  before_action :basic_authentication
   before_action :authenticate_user!, except: [:top]
 
   def after_sign_in_path_for(resource)
@@ -11,10 +11,10 @@ class ApplicationController < ActionController::Base
   root_path
   end
 
-  #private
-  #def basic_authentication
-    #authenticate_or_request_with_http_basic do |user_name, password|
-      #user_name ==ENV["USER_NAME"] && password == ENV["PASSWORD"]
-    #end
-  #end
+  private
+  def basic_authentication
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name ==ENV["USER_NAME"] && password == ENV["PASSWORD"]
+    end
+  end
 end
