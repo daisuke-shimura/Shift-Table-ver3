59d612979ba18d60f696680ce438d6d50ab2cca4
daisuke-shimura <daisuke.s0707@icloud.com>
2024-12-04 21:54:08 +0900

    excel 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,23 +32,27 @@ class UsersController < ApplicationController
 
     workbook.add_worksheet(name: "User_index") do |sheet|
       sheet.column_widths 14, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 12
-      sheet.merge_cells("C2:D2")
-      sheet.merge_cells("E2:F2")
-      sheet.merge_cells("G2:H2")
-      sheet.merge_cells("I2:J2")
-      sheet.merge_cells("K2:L2")
-      sheet.merge_cells("M2:N2")
-      sheet.merge_cells("O2:P2")
-      sheet.merge_cells("Q2:R2")
-      sheet.merge_cells("S2:T2")
-      sheet.merge_cells("U2:V2")
-      sheet.merge_cells("W2:X2")
-      sheet.merge_cells("Y2:Z2")
-      sheet.merge_cells("AA2:AB2")
+      sheet.merge_cells("C1:D1")
+      sheet.merge_cells("E1:F1")
+      sheet.merge_cells("G1:H1")
+      sheet.merge_cells("I1:J1")
+      sheet.merge_cells("K1:L1")
+      sheet.merge_cells("M1:N1")
+      sheet.merge_cells("O1:P1")
+      sheet.merge_cells("Q1:R1")
+      sheet.merge_cells("S1:T1")
+      sheet.merge_cells("U1:V1")
+      sheet.merge_cells("W1:X1")
+      sheet.merge_cells("Y1:Z1")
+      sheet.merge_cells("AA1:AB1")
       sheet.add_row ["#{Time.current.to_date.month}月#{Time.current.to_date.day}日（水）"]
+      #data_style = workbook.styles.add_style(sz: 8)
+      #sheet.add_row ["","","10","11","12","13","14","15","16","17","18","19","20","21","22"], style: data_style
+      
       User.all.each do |user|
         sheet.add_row [user.name]
       end
+
     end
 
     send_data package.to_stream.read, type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename: "users.xlsx"
