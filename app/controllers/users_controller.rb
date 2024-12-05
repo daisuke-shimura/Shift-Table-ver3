44a240d5f3559cac03913a3f1285b18da14c24ff
daisuke-shimura <daisuke.s0707@icloud.com>
2024-12-05 14:45:10 +0900

    excel 5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,18 +46,28 @@ class UsersController < ApplicationController
       merge_ranges.each { |range| sheet.merge_cells(range) }
 
       sheet.add_row ["#{Time.current.to_date.month}月#{Time.current.to_date.day}日（水）","","10","","11","","12","","13","","14","","15","","16","","17","","18","","19","","20","","21","","22"]
-
+      
+      #人の名前
       User.all.each do |user|
         sheet.add_row [user.name,"","","","","","","","","","","","","","","","","","","","","","","","","","","",""], style: center_style
       end
 
-      #青に塗りつぶし
-      sheet.rows[3].cells[2].style = blue_style
-      sheet.rows[3].cells[3].style = blue_style
+      shift_box = [[2,[1,2,3,4,5,6,7,8,9,10]],[3,[1,2,3,4,5,6],]]
+      shift_box.each do |i,k|
+        k.each do |j|
+          sheet.rows[i].cells[j].style = blue_style
+        end
+      end 
+
 
       #縦の太線
-      border_ranges = [2,4,6,8,10,12,14,16,18,20,22,24,26]
-      border_ranges.each { |range| sheet.col_style(range, border_style)}
+      #border_ranges = [2,4,6,8,10,12,14,16,18,20,22,24,26]
+      #border_ranges.each { |range| sheet.col_style(range, border_style)}
+
+      #青に塗りつぶし
+
+      sheet.rows[4].cells[2].style = blue_style
+      sheet.rows[5].cells[3].style = blue_style
 
       #中央揃え
       sheet.rows[1].style = center_style
