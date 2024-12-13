fcde52423a98f1cbcc73d3a2280459d923aab1cb
daisuke-shimura <daisuke.s0707@icloud.com>
2024-12-13 11:25:30 +0900

    excel 39

--- app/controllers/excel_controller.rb
+++ app/controllers/excel_controller.rb
@@ -10,34 +10,14 @@ class ExcelController < ApplicationController
 
     workbook.add_worksheet(name: "Sheet1") do |sheet|
 
-      thick_border_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: { style: :medium, color: '000000', edges: [:bottom] },
-        font_name: "AR丸ゴシック体M")
-      thick_top_border_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: { style: :medium, color: '000000', edges: [:bottom, :top] },
-        font_name: "AR丸ゴシック体M")
-
-      footer_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: { style: :medium, color: '000000', edges: [:bottom, :top] },
-        b: true,                   # 太字
-        font_name: "HGP創英角ﾎﾟｯﾌﾟ体"     # フォントの種類
-        )
-
-      name_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: [{ style: :thin, color: '000000', edges: [:right, :bottom] },
-                 { style: :medium, color: "000000", edges: [:left] }],
-        font_name: "AR丸ゴシック体M"
-      )
-      head_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: [{ style: :thin, color: '000000', edges: [:left, :bottom] },
-                 { style: :medium, color: "000000", edges: [:right] }],
-        font_name: "AR丸ゴシック体M"
-      )
+      sheet.page_margins do |margins|
+        margins.left = 0      # 左余白
+        margins.right = 0     # 右余白
+        margins.top = 0       # 上余白
+        margins.bottom = 0    # 下余白
+        margins.header = 0    # ヘッダー余白
+        margins.footer = 0    # フッター余白
+      end
 
       empty_row = Array.new(30, " ")
 
@@ -49,150 +29,156 @@ class ExcelController < ApplicationController
                   "#{(@day.start+5).month}月#{(@day.start+5).day}日（土）",
                   "#{(@day.start+6).month}月#{(@day.start+6).day}日（日）",]
       
-      sheet.page_margins do |margins|
-        margins.left = 0      # 左余白
-        margins.right = 0     # 右余白
-        margins.top = 0       # 上余白
-        margins.bottom = 0    # 下余白
-        margins.header = 0    # ヘッダー余白
-        margins.footer = 0    # フッター余白
-      end
+      event_day = [@day.mon_by?(@day.id),
+                   @day.tue_by?(@day.id),
+                   @day.wed_by?(@day.id),
+                   @day.thu_by?(@day.id),
+                   @day.fri_by?(@day.id),
+                   @day.sat_by?(@day.id)]
+      
+      style_method = style_box(workbook)
 
   #月曜日ここから
-    head_print(sheet, workbook, day_week, empty_row, 0)
-    shift_print(workbook, sheet, :time1, 0)
-  #月曜日ここまで
+    head_print(sheet, workbook, empty_row, day_week, event_day, style_method, 0)
+    shift_print(workbook, sheet, :time1, empty_row, style_method, 0)
 
   #火曜日
-    head_print(sheet, workbook, day_week, empty_row, 1)
-    shift_print(workbook, sheet, :time2, 1)
+    head_print(sheet, workbook, empty_row, day_week, event_day, style_method, 1)
+    shift_print(workbook, sheet, :time2, empty_row, style_method, 1)
 
   #水曜日
-    head_print(sheet, workbook, day_week, empty_row, 2)
-    shift_print(workbook, sheet, :time3, 2)
+    head_print(sheet, workbook, empty_row, day_week, event_day, style_method, 2)
+    shift_print(workbook, sheet, :time3, empty_row, style_method, 2)
 
   #木曜日
-    head_print(sheet, workbook, day_week, empty_row, 3)
-    shift_print(workbook, sheet, :time4, 3)
+    head_print(sheet, workbook, empty_row, day_week, event_day, style_method, 3)
+    shift_print(workbook, sheet, :time4, empty_row, style_method, 3)
 
   #金曜日
-    head_print(sheet, workbook, day_week, empty_row, 4)
-    shift_print(workbook, sheet, :time5, 4)
+    head_print(sheet, workbook, empty_row, day_week, event_day, style_method, 4)
+    shift_print(workbook, sheet, :time5, empty_row, style_method, 4)
 
   #土曜日
-    head_print(sheet, workbook, day_week, empty_row, 5)
-    shift_print(workbook, sheet, :time6, 5)
+    head_print(sheet, workbook, empty_row, day_week, event_day, style_method, 5)
+    shift_print(workbook, sheet, :time6, empty_row, style_method, 5)
 
   #日曜日
-    head_print(sheet, workbook, day_week, empty_row, 6)
-    shift_print(workbook, sheet, :time7, 6)
+    head_print(sheet, workbook, empty_row, day_week, event_day, style_method, 6)
+    shift_print(workbook, sheet, :time7, empty_row, style_method, 6)
     
-    sheet.add_row(empty_row, style: thick_top_border_style, height: 25)
+    sheet.add_row(empty_row, style: style_method[:border_style], height: 25)
     @row += 1
-    sheet.add_row(empty_row, style: footer_style, height: 25)
+    sheet.add_row(empty_row, style: style_method[:footer_style], height: 25)
     @row += 1
 
     sheet.rows[@row-1].cells[0].value = "#{(@day.start).year} シフト表"
-
     sheet.merge_cells("A#{@row}:AC#{@row}")
 
       #列の幅指定（最後）
       sheet.column_widths 13.5, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4, 11.5, 12
     end
-
     send_data package.to_stream.read, type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename: "#{@day.start.month}月#{@day.start.day}日～.xlsx"
-
   end
 
   private
 
-    def blue_style(x,workbook)
-      n2_border = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        bg_color: "4BACC6", 
-        border: [{ style: :medium, color: "000000", edges: [:right] },
-                { style: :thin, color: "000000", edges: [:bottom] }],
-        font_name: "AR丸ゴシック体M"
-      )
-      n1_border = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        bg_color: "4BACC6",
-        border: { style: :thin, color: "000000", edges: [:right, :bottom] },
-        font_name: "AR丸ゴシック体M"
-      )
-      if x % 2 == 0
-        n2_border
-      else
-        n1_border
-      end
+    def style_box(workbook)
+      {
+        top_style: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: { style: :medium,color: '000000', edges: [:top] },
+          font_name: "AR丸ゴシック体M"),
+
+        bottom_style: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: { style: :medium, color: '000000', edges: [:bottom] },
+          font_name: "AR丸ゴシック体M"),
+
+        border_style: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: { style: :medium, color: '000000', edges: [:bottom, :top] },
+          font_name: "AR丸ゴシック体M"),
+
+        footer_style: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: { style: :medium, color: '000000', edges: [:bottom, :top] },
+          b: true,
+          font_name: "HGP創英角ﾎﾟｯﾌﾟ体"),
+
+        manager_style: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: [{ style: :thin, color: '000000', edges: [:right, :bottom] },
+                  { style: :medium, color: "000000", edges: [:left] }],
+          fg_color: "00B050",
+          b: true,
+          font_name: "AR丸ゴシック体M"),
+        
+        name_style: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: [{ style: :thin, color: '000000', edges: [:right, :bottom] },
+                  { style: :medium, color: "000000", edges: [:left] }],
+          font_name: "AR丸ゴシック体M"),
+
+        blue_font: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: { style: :medium, color: '000000', edges: [:bottom] },
+          fg_color: "0070C0",
+          font_name: "AR丸ゴシック体M"),
+
+        red_font: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: { style: :medium, color: '000000', edges: [:bottom] },
+          fg_color: "FF0000",
+          font_name: "AR丸ゴシック体M"),
+
+        #塗りつぶしメソッド
+        blue_n2: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          bg_color: "4BACC6", 
+          border: [{ style: :medium, color: "000000", edges: [:right] },
+                  { style: :thin, color: "000000", edges: [:bottom] }],
+          font_name: "AR丸ゴシック体M"
+        ),
+        blue_n1: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          bg_color: "4BACC6",
+          border: { style: :thin, color: "000000", edges: [:right, :bottom] },
+          font_name: "AR丸ゴシック体M"
+        ),
+
+        white_n2: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: [{ style: :medium, color: "000000", edges: [:right] },
+                  { style: :thin, color: "000000", edges: [:bottom] }],
+          font_name: "AR丸ゴシック体M"
+        ),
+        white_n1: workbook.styles.add_style(
+          alignment: { horizontal: :center ,vertical: :center},
+          border: { style: :thin, color: "000000", edges: [:right, :bottom] },
+          font_name: "AR丸ゴシック体M"
+        ),
+      }
     end
 
-    def white_style(x,workbook)
-      n2_border = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: [{ style: :medium, color: "000000", edges: [:right] },
-                { style: :thin, color: "000000", edges: [:bottom] }],
-        font_name: "AR丸ゴシック体M"
-      )
-      n1_border = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: { style: :thin, color: "000000", edges: [:right, :bottom] },
-        font_name: "AR丸ゴシック体M"
-      )
-      if x % 2 == 0
-        n2_border
-      else
-        n1_border
-      end
-    end
-
-
-    def head_print(sheet, workbook, day_week, empty_row, week_n)
-
-      thick_border_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: { style: :medium, color: '000000', edges: [:bottom] },
-        font_name: "AR丸ゴシック体M")
-
-      thick_top_border_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: { style: :medium,color: '000000', edges: [:bottom, :top] },
-        font_name: "AR丸ゴシック体M")
-
-      top_only_border_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: { style: :medium,color: '000000', edges: [:top] },
-        font_name: "AR丸ゴシック体M")
-
-      blue_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: { style: :medium, color: '000000', edges: [:bottom] },
-        fg_color: "0070C0",
-        font_name: "AR丸ゴシック体M")
-
-      red_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: { style: :medium, color: '000000', edges: [:bottom] },
-        fg_color: "FF0000",
-        font_name: "AR丸ゴシック体M")
+    def head_print(sheet, workbook, empty_row, day_week, event_day, style_method, week_n)
 
       if week_n == 0
-        sheet.add_row(empty_row, style: thick_border_style, height: 7)
+        sheet.add_row(empty_row, style: style_method[:bottom_style], height: 7)
         @row += 1
       elsif week_n == 4
-        sheet.add_row(empty_row, style: top_only_border_style)#印刷分かれる
+        sheet.add_row(empty_row, style: style_method[:top_style])#印刷分かれる
         @row += 1
-        sheet.add_row(empty_row, style: thick_border_style, height: 20)
+        sheet.add_row(empty_row, style: style_method[:bottom_style], height: 20)
         @row += 1
       elsif week_n == 5 || week_n == 6
-        sheet.add_row(empty_row, style: thick_top_border_style, height: 30)
+        sheet.add_row(empty_row, style: style_method[:border_style], height: 30)
         @row += 1
       else
-        sheet.add_row(empty_row, style: thick_top_border_style, height: 8)
+        sheet.add_row(empty_row, style: style_method[:border_style], height: 8)
         @row += 1
       end
 
-      sheet.add_row(empty_row, style: thick_border_style, height: 16.5)
+      sheet.add_row(empty_row, style: style_method[:bottom_style], height: 16.5)
         @row += 1
 
       merge_ranges = ["C#{@row}:D#{@row}", "E#{@row}:F#{@row}", "G#{@row}:H#{@row}", "I#{@row}:J#{@row}", "K#{@row}:L#{@row}",
@@ -205,34 +191,18 @@ class ExcelController < ApplicationController
         sheet.rows[@row-1].cells[i].value = value
       end
 
-      if week_n == 0
-        if @day.mon_by?(@day.id)
-          sheet.rows[@row-1].cells[0].style = red_style
-        end
-      elsif week_n == 1
-        if @day.tue_by?(@day.id)
-          sheet.rows[@row-1].cells[0].style = red_style
-        end
-      elsif week_n == 2
-        if @day.wed_by?(@day.id)
-          sheet.rows[@row-1].cells[0].style = red_style
-        end
-      elsif week_n == 3
-        if @day.thu_by?(@day.id)
-          sheet.rows[@row-1].cells[0].style = red_style
-        end
-      elsif week_n == 4
-        if @day.fri_by?(@day.id)
-          sheet.rows[@row-1].cells[0].style = red_style
+      if week_n <= 4
+        if event_day[week_n]
+          sheet.rows[@row-1].cells[0].style = style_method[:red_font]
         end
       elsif week_n == 5
-        if @day.sat_by?(@day.id)
-          sheet.rows[@row-1].cells[0].style = red_style
+        if event_day[week_n]
+          sheet.rows[@row-1].cells[0].style = style_method[:red_font]
         else
-          sheet.rows[@row-1].cells[0].style = blue_style
+          sheet.rows[@row-1].cells[0].style = style_method[:blue_font]
         end
       elsif week_n == 6
-        sheet.rows[@row-1].cells[0].style = red_style
+        sheet.rows[@row-1].cells[0].style = style_method[:red_font]
       end
 
       (2..26).step(2).each { |int| sheet.rows[@row-1].cells[int].type = :integer }
@@ -240,22 +210,8 @@ class ExcelController < ApplicationController
     end
 
 
-    def shift_print(workbook, sheet, time_column, week_n)
-      name_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: [{ style: :thin, color: '000000', edges: [:right, :bottom] },
-                 { style: :medium, color: "000000", edges: [:left] }],
-        font_name: "AR丸ゴシック体M"
-      )
-      manager_style = workbook.styles.add_style(
-        alignment: { horizontal: :center ,vertical: :center},
-        border: [{ style: :thin, color: '000000', edges: [:right, :bottom] },
-                 { style: :medium, color: "000000", edges: [:left] }],
-        fg_color: "00B050",
-        b: true,
-        font_name: "AR丸ゴシック体M"
-      )
-      empty_row = Array.new(30, " ")
+    def shift_print(workbook, sheet, time_column, empty_row, style_method, week_n)
+
       shift_box = []
       shift_box[0] = [1 ,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,""]
       #抜き取る
@@ -303,7 +259,7 @@ class ExcelController < ApplicationController
       end
 
       userid.times do
-        sheet.add_row(empty_row, height: 16.5)#, style: name_style
+        sheet.add_row(empty_row, height: 16.5)
         @row += 1
       end
 
@@ -325,11 +281,11 @@ class ExcelController < ApplicationController
       times.each_with_index do |k,n|
         k.each_with_index do |i,j|
           if i == 5 || i == 30
-            y = (2*k[j-1])-18 +1 # +1 IDつけたから
+            y = (2*k[j-1])-17
             shift_box[n+1][y] = false
             shift_box[n+1][y+1] = true
           elsif 9 <= i && i <= 21
-            y = (2*i)-18 +1 # +1 IDつけたから
+            y = (2*i)-17
             shift_box[n+1][y] = true
           end
         end
@@ -358,24 +314,33 @@ class ExcelController < ApplicationController
           end
         end
       end
+
       shift_box.each_with_index do |k,i|
         k.each_with_index do |j,x|
-            if j == true
-              sheet.rows[@row - userid + i].cells[x].style = blue_style(x,workbook)
-            elsif j == false
-              sheet.rows[@row - userid + i].cells[x].style = white_style(x,workbook)
-            elsif j.is_a?(Integer)
-              sheet.rows[@row - userid + i].cells[x].value = User.find(j).name
-              if j == 1
-                sheet.rows[@row - userid + i].cells[x].style = manager_style
-              else
-                sheet.rows[@row - userid + i].cells[x].style = name_style
-              end
-            elsif x == 29
-              sheet.rows[@row - userid + i].cells[x].value = j
+          if j == true
+            if x % 2 == 0
+              sheet.rows[@row - userid + i].cells[x].style = style_method[:blue_n2]
             else
-              sheet.rows[@row - userid + i].cells[x].style = name_style
+              sheet.rows[@row - userid + i].cells[x].style = style_method[:blue_n1]
             end
+          elsif j == false
+            if x % 2 == 0
+              sheet.rows[@row - userid + i].cells[x].style = style_method[:white_n2]
+            else
+              sheet.rows[@row - userid + i].cells[x].style = style_method[:white_n1]
+            end
+          elsif j.is_a?(Integer)
+            sheet.rows[@row - userid + i].cells[x].value = User.find(j).name
+            if j == 1
+              sheet.rows[@row - userid + i].cells[x].style = style_method[:manager_style]
+            else
+              sheet.rows[@row - userid + i].cells[x].style = style_method[:name_style]
+            end
+          elsif x == 29
+            sheet.rows[@row - userid + i].cells[x].value = j
+          else
+            sheet.rows[@row - userid + i].cells[x].style = style_method[:name_style]
+          end
         end
       end
 
